59f839b3a792ca35538cfc8833bdf935646c2a0f
Lawrence Ephrim <ephrimlawrence@gmail.com>
2022-12-06 19:34:35 +0000

    feat: add more tests

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -4,6 +4,7 @@ import { Todo, TodoSchema } from './entities/todo.entity';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { faker } from '@faker-js/faker';
+import { NotFoundException } from '@nestjs/common';
 
 describe('TodosController', () => {
   let controller: TodosController;
@@ -26,12 +27,67 @@ describe('TodosController', () => {
   });
 
   it('should create a todo item', async () => {
-    const resp: any = await controller.create({
+    const body = {
       title: faker.random.words(),
       body: faker.lorem.paragraphs(),
-    });
+    };
+    const resp: any = await controller.create(body);
 
-    console.log(resp);
     expect(resp.id).toBeDefined();
+    expect(resp.updatedAt).toBeDefined();
+    expect(resp.createdAt).toBeDefined();
+    expect(resp.title).toEqual(body.title);
+    expect(resp.body).toEqual(body.body);
+  });
+
+  it('create todo with existing title', async () => {
+    const body = {
+      title: faker.random.words(),
+      body: faker.lorem.paragraphs(),
+    };
+    const resp: any = await controller.create(body);
+    expect(resp.id).toBeDefined();
+
+    try {
+      await controller.create(body);
+    } catch (error) {
+      expect(error.message).toBe(
+        'A todo with similar title already exists! Not title should be unique',
+      );
+    }
+  });
+
+  it('should deleted a todo item', async () => {
+    const body = {
+      title: faker.random.words(),
+      body: faker.lorem.paragraphs(),
+    };
+    const resp: any = await controller.create(body);
+
+    const deleted: any = await controller.remove(resp.id.toString());
+    expect(deleted.id).toBeDefined();
+
+    try {
+      await controller.findOne(resp.id.toString());
+    } catch (error) {
+      expect(error.message).toBe('Oops! Todo item cannot be found');
+    }
+  });
+
+  it('should update a todo item', async () => {
+    const body = {
+      title: faker.random.words(),
+      body: faker.lorem.paragraphs(),
+    };
+    const resp: any = await controller.create(body);
+    expect(resp.id).toBeDefined();
+
+    const body2 = { title: body.title, body: faker.lorem.paragraphs() };
+    const updated: any = await controller.update(resp.id.toString(), body2);
+    expect(updated.id).toBeDefined();
+    expect(updated.updatedAt).toBeDefined();
+    expect(updated.createdAt).toBeDefined();
+    expect(updated.title).toEqual(body2.title);
+    expect(updated.body).toEqual(body2.body);
   });
 });
