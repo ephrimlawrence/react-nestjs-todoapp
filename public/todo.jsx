029f70a20950d9bfa6052ac70754f817f59d199a
Lawrence Ephrim <ephrimlawrence@gmail.com>
2022-12-06 10:50:02 +0000

    feat: update ui

--- public/todo.jsx
+++ public/todo.jsx
@@ -2,7 +2,7 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class TodoApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = { liked: false };
@@ -13,15 +13,43 @@ class LikeButton extends React.Component {
             return 'You liked this.';
         }
 
-        return <button className="btn btn-primary">Working</button>
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like',
-        );
+        return <div className="d-flex justify-content-center">
+            <div className="row row-cols-1 row-cols-md-1 g-4">
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">
+                            Title here
+                        </div>
+
+                        <div className="card-body">
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                        <div className="card-footer text-muted">
+                            <div className="row">
+                                <div className="col-md-10">
+                                    updated at <br />
+                                    created at
+                                </div>
+                                <div className="col-md-2">
+                                    <button>Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        // return <button className="btn btn-primary">Working</button>
+        // return e(
+        //     'button',
+        //     { onClick: () => this.setState({ liked: true }) },
+        //     'Like',
+        // );
     }
 }
 
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector('#app');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(e(TodoApp));
