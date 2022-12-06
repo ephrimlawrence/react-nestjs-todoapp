6330d12e23a976162111a1b31cfff4f0b65639c5
Lawrence Ephrim <ephrimlawrence@gmail.com>
2022-12-06 18:56:39 +0000

    fix: update todo card footer

--- public/todo.jsx
+++ public/todo.jsx
@@ -37,20 +37,8 @@ function NewTodoItem(props) {
         }
     }, [props]);
 
-    // static getDerivedStateFromProps(props, state) {
-    //     if (state.resetForm == true) {
-    //         return { onItemUpdated: state.onItemUpdated, title: '', body: '', isUpdate: false, id: '', resetForm: false }
-    //     }
-    //     if (state.id == props.id) {
-    //         return { title: state.title || '', body: state.body || '', isUpdate: state.isUpdate || false, id: state.id, onItemUpdated: state.onItemUpdated };
-
-    //     }
-    //     return { title: props.title || '', body: props.body || '', isUpdate: props.isUpdate || false, id: props.id, onItemUpdated: props.onItemUpdated };
-    // }
-
     const postData = async () => {
         // TODO: throw error if input is empty
-        // console.log(this.state)
         const url = `/api/todos/${isUpdate ? id : ''}`
         const response = await fetch(url, {
             method: isUpdate ? 'PUT' : 'POST',
@@ -81,17 +69,12 @@ function NewTodoItem(props) {
             <div className="mb-3">
                 <label htmlFor="title" className="form-label">Title</label>
                 <input type="text" className="form-control" value={title} id="title" placeholder="eg. Grocery shopping" onChange={(e) => {
-                    // console.log(e.target.value)
                     setTitle(e.target.value)
-                    // this.setState({ title: e.target.value })
-
-
                 }} />
             </div>
             <div className="mb-3">
                 <label htmlFor="body" className="form-label">Body</label>
                 <textarea className="form-control" id="body" value={body} rows="3" onChange={(e) => {
-                    // this.setState({ body: e.target.value })
                     setBody(e.target.value);
 
                     if (isUpdate == true) {
@@ -107,6 +90,8 @@ function NewTodoItem(props) {
 
 
 class TodoApp extends React.Component {
+    dateFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
     constructor(props) {
         super(props);
         this.state = {
@@ -193,17 +178,27 @@ class TodoApp extends React.Component {
                                         </div>
                                         <div className="card-footer text-muted">
                                             <div className="row">
-                                                <div className="col-md-10">
-                                                    updated at <br />
-                                                    created at
+                                                <div className="col-md-10 d-block">
+                                                    updated @ {new Date(item.updatedAt).toLocaleDateString("en-US", this.dateFormatOptions)} <br />
+
+                                                    created @ {new Date(item.createdAt).toLocaleDateString("en-US", this.dateFormatOptions)}
                                                 </div>
-                                                <div className="col-md-2 btn-group">
-                                                    <button className="btn btn-info btn-sm float-start" onClick={() => {
+                                                <div className="col-md-2 text-center">
+                                                    <button className="btn btn-primary btn-sm me-3" onClick={() => {
                                                         this.setState({ selectedItem: item });
                                                         this.setState({ isUpdate: true });
-                                                    }}>Update</button>
+                                                    }} title="Update todo item">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                                                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                                                        </svg>
+                                                    </button>
 
-                                                    <button className="btn btn-danger btn-sm float-end" onClick={(e) => this.deleteItem(item.id)}>Delete</button>
+                                                    <button className="btn btn-danger btn-sm" onClick={(e) => this.deleteItem(item.id)} title="Delete todo item">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                        </svg>
+                                                    </button>
                                                 </div>
                                             </div>
                                         </div>
